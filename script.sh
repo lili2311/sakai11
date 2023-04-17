202f07851c6c28365e18e86073398ebd995de450
ghe <lili@snyk.io>
2023-04-17 13:07:27 +0100

    chore: a comment every 20 seconds

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-for i in {1..40}; do find . -name pom.xml | while read file; do echo "<--no comment -->" >> $file; done && git add . && git commit -m "chore: a comment" && git push -u origin -f feat/using-workspaces; date ; sleep 20; done
+for i in {1..40}; do find . -name pom.xml | while read file; do echo "<--no comment -->" >> $file; done && git add . && git commit -m "chore: a comment every 20 seconds" && git push -u origin -f feat/using-workspaces; date ; sleep 20; done
 
