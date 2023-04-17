2a6037f81224cbcf32e5880b38343df40b418db1
ghe <lili@snyk.io>
2023-04-17 13:06:39 +0100

    chore: a comment

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
-find . -name pom.xml | while read file; do echo "<--no comment -->" >> $file; done && git add . && git commit -m "chore: a comment" && git push -u origin -f feat/using-workspaces
+for i in {1..40}; do find . -name pom.xml | while read file; do echo "<--no comment -->" >> $file; done && git add . && git commit -m "chore: a comment" && git push -u origin -f feat/using-workspaces; date ; sleep 5; done
+
